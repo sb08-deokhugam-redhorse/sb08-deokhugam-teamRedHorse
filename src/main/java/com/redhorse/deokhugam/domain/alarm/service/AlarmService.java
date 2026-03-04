5dcb0b4a56c4c8118dd86e952dd14ea7ea41cfe7
dev-jin8612 <workspace8612@gmail.com>
2026-03-04 16:13:52 +0900

    refactor: 알림 엔티티, DDL 변경3

--- src/main/java/com/redhorse/deokhugam/domain/alarm/service/AlarmService.java
+++ src/main/java/com/redhorse/deokhugam/domain/alarm/service/AlarmService.java
@@ -1,15 +1,15 @@
-package com.redhorse.deokhugam.domain.alarm.service;
-
-import com.redhorse.deokhugam.domain.alarm.dto.NotificationDto;
-import com.redhorse.deokhugam.domain.comment.dto.CommentDto;
-import com.redhorse.deokhugam.domain.review.dto.ReviewLikeDto;
-
-public interface AlarmService {
-    NotificationDto createCommentAlarm(CommentDto dto);
-    NotificationDto createLikeAlarm(ReviewLikeDto dto);
-    NotificationDto createDashboardAlarm();
-    void checkAlarm();
-    void checkAllAlarm();
-    void deleteAlarm();
-    void getAlarmList();
-}
+//package com.redhorse.deokhugam.domain.alarm.service;
+//
+//import com.redhorse.deokhugam.domain.alarm.dto.NotificationDto;
+//import com.redhorse.deokhugam.domain.comment.dto.CommentDto;
+//import com.redhorse.deokhugam.domain.review.dto.ReviewLikeDto;
+//
+//public interface AlarmService {
+//    NotificationDto createCommentAlarm(CommentDto dto);
+//    NotificationDto createLikeAlarm(ReviewLikeDto dto);
+//    NotificationDto createDashboardAlarm();
+//    void checkAlarm();
+//    void checkAllAlarm();
+//    void deleteAlarm();
+//    void getAlarmList();
+//}
