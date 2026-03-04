30243aa198314032dd430dfb9276389427bf6744
castle-bird <seongjo0501@gmail.com>
2026-03-04 11:39:13 +0900

    feat: Enum ErrorCode에 중복 사용자 에러코드 추가

--- src/main/java/com/redhorse/deokhugam/global/exception/ErrorCode.java
+++ src/main/java/com/redhorse/deokhugam/global/exception/ErrorCode.java
@@ -6,7 +6,8 @@ import org.springframework.http.HttpStatus;
 @Getter
 public enum ErrorCode {
     // user
-    USER_NOT_FOUND("사용자를 찾을 수 없습니다.", HttpStatus.NOT_FOUND);
+    USER_NOT_FOUND("사용자를 찾을 수 없습니다.", HttpStatus.NOT_FOUND),
+    USER_DUPLICATE("이미 존재하는 사용자입니다.", HttpStatus.CONFLICT);
 
     // review
 
