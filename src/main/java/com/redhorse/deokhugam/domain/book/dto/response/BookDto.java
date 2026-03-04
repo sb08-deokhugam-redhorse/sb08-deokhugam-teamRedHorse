e9218227852c0a88446c43eb0dfc48eb8235a725
xxzeroeight <xxzeroeight@naver.com>
2026-03-04 11:50:28 +0900

    feature: 기본 Dto 작성
    - BookCreateRequest, BookUpdateRequest
    - BookDto

---
+++ src/main/java/com/redhorse/deokhugam/domain/book/dto/response/BookDto.java
@@ -0,0 +1,20 @@
+package com.redhorse.deokhugam.domain.book.dto.response;
+
+import java.time.Instant;
+import java.time.LocalDate;
+import java.util.UUID;
+
+public record BookDto(
+        UUID id,
+        String title,
+        String author,
+        String description,
+        String publisher,
+        LocalDate publishedDate,
+        String isbn,
+        String thumbnailUrl,
+        Long reviewCount,
+        double rating,
+        Instant createdAt,
+        Instant updatedAt
+) {}
