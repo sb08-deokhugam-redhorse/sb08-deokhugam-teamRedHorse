d3915ec6d91095693193ec6cd1be5f93ababcf30
parkhamin <parkhamint@naver.com>
2026-03-05 20:44:38 +0900

    test: 테스트 프로파일 적용

--- src/test/java/com/redhorse/deokhugam/domain/comment/controller/CommentControllerTest.java
+++ src/test/java/com/redhorse/deokhugam/domain/comment/controller/CommentControllerTest.java
@@ -25,9 +25,11 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
+@ActiveProfiles("test")
 @WebMvcTest(CommentController.class)
 @DisplayName("CommentController 테스트")
 class CommentControllerTest {
