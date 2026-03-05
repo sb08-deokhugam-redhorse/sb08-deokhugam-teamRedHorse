1c39ac353a9d2c411bb7a7152ac822d5e0494162
castle-bird <seongjo0501@gmail.com>
2026-03-05 19:58:50 +0900

    refactor: 인터셉터에서 Repository삭제
    - 안티패턴 제거

--- src/main/java/com/redhorse/deokhugam/global/config/AuthenticationInterceptor.java
+++ src/main/java/com/redhorse/deokhugam/global/config/AuthenticationInterceptor.java
@@ -1,6 +1,5 @@
 package com.redhorse.deokhugam.global.config;
 
-import com.redhorse.deokhugam.domain.user.repository.UserRepository;
 import com.redhorse.deokhugam.global.exception.AuthenticationException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -12,8 +11,6 @@ import org.springframework.web.servlet.HandlerInterceptor;
 @Component
 @RequiredArgsConstructor
 public class AuthenticationInterceptor implements HandlerInterceptor {
-
-  private final UserRepository userRepository;
 
   @Override
   public boolean preHandle(
@@ -28,19 +25,13 @@ public class AuthenticationInterceptor implements HandlerInterceptor {
       throw new AuthenticationException();
     }
 
-    UUID userId;
-
     try {
-      // String → UUID 변환할 때
-      // String이 UUID 형식이 아니면 IllegalArgumentException 에러나기때문에 try문 사용
-      userId = UUID.fromString(userIdHeader);
+      // UUID 패턴인지 확인
+      // String → UUID 변환할 때, String이 UUID 형식이 아니면 IllegalArgumentException 에러 발생하기 때문에 try문 사용
+      UUID.fromString(userIdHeader);
     } catch (IllegalArgumentException e) {
       throw new AuthenticationException();
     }
-
-    // 실제 유저가 존재하는지 확인
-    userRepository.findById(userId)
-        .orElseThrow(AuthenticationException::new);
 
     return true;
   }
