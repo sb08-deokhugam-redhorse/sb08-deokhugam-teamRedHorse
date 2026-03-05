1f0b58458b7bf4048c1a53df4d9bf60ca3f518ca
dev-jin8612 <workspace8612@gmail.com>
2026-03-05 17:29:38 +0900

    feat: 알림 상태 업데이트 기능 & 테스트 코드 구현2

--- src/test/java/com/redhorse/deokhugam/domain/alarm/service/AlarmServiceAlarmReadUnitTest.java
+++ src/test/java/com/redhorse/deokhugam/domain/alarm/service/AlarmServiceAlarmReadUnitTest.java
@@ -107,4 +107,36 @@ class AlarmServiceAlarmReadUnitTest {
                 .isInstanceOf(NoAlarmException.class)
                 .hasMessage("알림을 찾을 수 없습니다.");
     }
+
+    @Test
+    @DisplayName("스프링 빈 환경에서 checkAllAlarm 호출 시 정상적으로 전체 업데이트가 수행된다")
+    void checkAllAlarm_InSpringContext() {
+        // given
+        UUID testUserId = UUID.randomUUID();
+        Alarm mockAlarm = mock(Alarm.class);
+        given(mockAlarm.getMessage()).willReturn("스프링 테스트 알림");
+
+        given(alarmRepository.findAllAlarmByUserId(testUserId)).willReturn(List.of(mockAlarm));
+
+        // when
+        alarmService.checkAllAlarm(testUserId);
+
+        // then
+        verify(mockAlarm).update();
+    }
+
+    @Test
+    @DisplayName("스프링 빈 환경에서 알림 단건 조회 실패 시 예외가 발생한다")
+    void checkAlarm_NotFound_InSpringContext() {
+        // given
+        UUID testAlarmId = UUID.randomUUID();
+        UUID testUserId = UUID.randomUUID();
+
+        given(alarmRepository.findById(testAlarmId)).willReturn(Optional.empty());
+
+        // when & then
+        assertThatThrownBy(() -> alarmService.checkAlarm(testAlarmId, testUserId))
+                .isInstanceOf(AlarmNotFoundException.class)
+                .hasMessage("알림을 찾을 수 없습니다.");
+    }
 }
