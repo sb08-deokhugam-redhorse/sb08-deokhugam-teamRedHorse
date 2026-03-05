5c2444672c3d9c443166a0da5295f1959c1d7989
dev-jin8612 <workspace8612@gmail.com>
2026-03-05 17:35:41 +0900

    feat: 알림 상태 업데이트 기능 & 테스트 코드 구현2

--- src/test/java/com/redhorse/deokhugam/domain/alarm/service/AlarmServiceAlarmReadUnitTest.java
+++ src/test/java/com/redhorse/deokhugam/domain/alarm/service/AlarmServiceAlarmReadUnitTest.java
@@ -109,7 +109,7 @@ class AlarmServiceAlarmReadUnitTest {
     }
 
     @Test
-    @DisplayName("스프링 빈 환경에서 checkAllAlarm 호출 시 정상적으로 전체 업데이트가 수행된다")
+    @DisplayName("checkAllAlarm 호출 시 정상적으로 전체 업데이트가 수행된다")
     void checkAllAlarm_InSpringContext() {
         // given
         UUID testUserId = UUID.randomUUID();
@@ -121,20 +121,5 @@ class AlarmServiceAlarmReadUnitTest {
 
         // then
         verify(mockAlarm).update();
-    }
-
-    @Test
-    @DisplayName("스프링 빈 환경에서 알림 단건 조회 실패 시 예외가 발생한다")
-    void checkAlarm_NotFound_InSpringContext() {
-        // given
-        UUID testAlarmId = UUID.randomUUID();
-        UUID testUserId = UUID.randomUUID();
-
-        given(alarmRepository.findById(testAlarmId)).willReturn(Optional.empty());
-
-        // when & then
-        assertThatThrownBy(() -> alarmService.checkAlarm(testAlarmId, testUserId))
-                .isInstanceOf(AlarmNotFoundException.class)
-                .hasMessage("알림을 찾을 수 없습니다.");
     }
 }
