fa80cecbc60047d096570f6445c73acde5f3d70b
castle-bird <seongjo0501@gmail.com>
2026-03-05 13:25:01 +0900

    test: 로그인 기능에 대한 Controller, Service, Repository 테스트 작성

--- src/test/java/com/redhorse/deokhugam/domain/user/controller/UserControllerTest.java
+++ src/test/java/com/redhorse/deokhugam/domain/user/controller/UserControllerTest.java
@@ -1,5 +1,6 @@
 package com.redhorse.deokhugam.domain.user.controller;
 
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
@@ -7,12 +8,18 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.redhorse.deokhugam.domain.user.dto.request.UserLoginRequest;
 import com.redhorse.deokhugam.domain.user.dto.request.UserRegisterRequest;
 import com.redhorse.deokhugam.domain.user.dto.response.UserDto;
 import com.redhorse.deokhugam.domain.user.exception.UserDuplicateException;
+import com.redhorse.deokhugam.domain.user.exception.UserLoginFailedException;
+import com.redhorse.deokhugam.domain.user.repository.UserRepository;
 import com.redhorse.deokhugam.domain.user.service.UserService;
+import com.redhorse.deokhugam.global.config.AuthenticationInterceptor;
+import com.redhorse.deokhugam.global.config.MDCLoggingInterceptor;
 import java.time.Instant;
 import java.util.UUID;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -33,6 +40,26 @@ class UserControllerTest {
 
   @MockitoBean
   private UserService userService;
+
+  @MockitoBean
+  private UserRepository userRepository;
+
+  @MockitoBean
+  private MDCLoggingInterceptor mdcLoggingInterceptor;
+
+  @MockitoBean
+  private AuthenticationInterceptor authenticationInterceptor;
+
+  @BeforeEach
+  void setUp() throws Exception {
+    given(
+        mdcLoggingInterceptor.preHandle(any(), any(), any())
+    ).willReturn(true);
+
+    given(
+        authenticationInterceptor.preHandle(any(), any(), any())
+    ).willReturn(true);
+  }
 
   @Test
   @DisplayName("유저 회원가입 성공")
@@ -124,5 +151,94 @@ class UserControllerTest {
         .content(objectMapper.writeValueAsString(request)));
 
     result.andExpect(status().isConflict());
+  }
+
+  @Test
+  @DisplayName("로그인 성공")
+  void login_Success() throws Exception {
+    // given
+    UserLoginRequest request = new UserLoginRequest(
+        "seongjo.park@gmail.com",
+        "Thisistest123***"
+    );
+
+    UserDto response = new UserDto(
+        UUID.randomUUID(),
+        request.email(),
+        "박성조",
+        Instant.now()
+    );
+
+    given(
+        userService.login(eq(request))
+    ).willReturn(response);
+
+    // when
+    var result = mockMvc.perform(post("/api/users/login")
+        .contentType(MediaType.APPLICATION_JSON)
+        .content(objectMapper.writeValueAsString(request)));
+
+    // then
+    result.andExpect(status().isOk())
+        .andExpect(jsonPath("$.email").value(request.email()))
+        .andExpect(jsonPath("$.nickname").value(response.nickname()));
+  }
+
+  @Test
+  @DisplayName("로그인 실패 - 잘못된 이메일 형식")
+  void login_InvalidEmail() throws Exception {
+    // given
+    UserLoginRequest request = new UserLoginRequest(
+        "seongjo.park",
+        "Thisistest123***"
+    );
+
+    // when
+    var result = mockMvc.perform(post("/api/users/login")
+        .contentType(MediaType.APPLICATION_JSON)
+        .content(objectMapper.writeValueAsString(request)));
+
+    // then
+    result.andExpect(status().isBadRequest());
+  }
+
+  @Test
+  @DisplayName("로그인 실패 - 필수값 누락")
+  void login_BlankField() throws Exception {
+    // given
+    UserLoginRequest request = new UserLoginRequest(
+        "",
+        "Thisistest123***"
+    );
+
+    // when
+    var result = mockMvc.perform(post("/api/users/login")
+        .contentType(MediaType.APPLICATION_JSON)
+        .content(objectMapper.writeValueAsString(request)));
+
+    // then
+    result.andExpect(status().isBadRequest());
+  }
+
+  @Test
+  @DisplayName("로그인 실패 - 계정 정보 불일치")
+  void login_Failed() throws Exception {
+    // given
+    UserLoginRequest request = new UserLoginRequest(
+        "wrong@gmail.com",
+        "wrongpassword"
+    );
+
+    given(
+        userService.login(eq(request))
+    ).willThrow(new UserLoginFailedException());
+
+    // when
+    var result = mockMvc.perform(post("/api/users/login")
+        .contentType(MediaType.APPLICATION_JSON)
+        .content(objectMapper.writeValueAsString(request)));
+
+    // then
+    result.andExpect(status().isUnauthorized());
   }
 }
