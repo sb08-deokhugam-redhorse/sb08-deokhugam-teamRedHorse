eb715877f9b431426b4744ff537a6bcf7f49b20f
dev-jin8612 <workspace8612@gmail.com>
2026-03-06 16:14:12 +0900

    feat: 알림 삭제 및 테스트 구현3

--- src/test/java/com/redhorse/deokhugam/batch/AlarmsCleanupTaskletUnitTest.java
+++ src/test/java/com/redhorse/deokhugam/batch/AlarmsCleanupTaskletUnitTest.java
@@ -1,7 +1,7 @@
 package com.redhorse.deokhugam.batch;
 
-import com.redhorse.deokhugam.domain.alarm.repository.AlarmRepository;
 import com.redhorse.deokhugam.global.batch.BatchConfig.AlarmsCleanupBatchConfig;
+import com.redhorse.deokhugam.global.batch.repository.AlarmBatchRepository;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -23,7 +23,7 @@ import static org.mockito.Mockito.*;
 class AlarmsCleanupTaskletUnitTest {
 
     @Mock
-    private AlarmRepository alarmRepository;
+    private AlarmBatchRepository alarmRepository;
 
     @InjectMocks
     private AlarmsCleanupBatchConfig batchConfig;
