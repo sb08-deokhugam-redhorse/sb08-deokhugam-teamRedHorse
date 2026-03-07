cc5fa7e5a45569add50900261439c6dabe8e84dc
castle-bird <seongjo0501@gmail.com>
2026-03-07 11:33:51 +0900

    refactor: 코드리뷰 반영

--- src/main/java/com/redhorse/deokhugam/domain/user/dto/request/UserUpdateRequest.java
+++ src/main/java/com/redhorse/deokhugam/domain/user/dto/request/UserUpdateRequest.java
@@ -4,8 +4,8 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
 public record UserUpdateRequest(
-    @NotBlank(message = "닉네임 필수 입력값입니다.")
-    @Size(min = 2, max = 20, message = "닉네임 2 ~ 20자 이내여야 합니다.")
+    @NotBlank(message = "닉네임은 필수 입력값입니다.")
+    @Size(min = 2, max = 20, message = "닉네임은 2 ~ 20자 이내여야 합니다.")
     String nickname
 ) {
 
