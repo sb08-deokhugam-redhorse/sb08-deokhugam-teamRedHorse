8dd6909637a1a20beae5a6632647655885f8bccb
dev-jin8612 <workspace8612@gmail.com>
2026-03-07 14:01:35 +0900

    feat: 알림 목록조회 구현 refactor: 안쓰는 코드 삭제 test: 알림 목록 조회 테스트 코드 추가

--- src/test/java/com/redhorse/deokhugam/domain/alarm/service/AlarmServiceGetListTest.java
+++ src/test/java/com/redhorse/deokhugam/domain/alarm/service/AlarmServiceGetListTest.java
@@ -47,21 +47,25 @@ class AlarmServiceGetListTest {
     void getAlarmList_WithNextPage() {
         // given
         UUID userId = UUID.randomUUID();
-        NotificationListRequest request = new NotificationListRequest(userId, "DESC", null, null, 2);
+        int limit = 2;
+        NotificationListRequest request = new NotificationListRequest(userId, "DESC", null, null, limit);
 
         Alarm alarm1 = mock(Alarm.class);
         Alarm alarm2 = mock(Alarm.class);
+        Alarm alarm3 = mock(Alarm.class);
+
         UUID lastId = UUID.randomUUID();
         Instant lastCreatedAt = Instant.now().minusSeconds(100);
 
         given(alarm2.getId()).willReturn(lastId);
         given(alarm2.getCreatedAt()).willReturn(lastCreatedAt);
 
-        List<Alarm> alarmList = List.of(alarm1, alarm2);
-        Slice<Alarm> alarmSlice = new SliceImpl<>(alarmList, PageRequest.of(0, 2), true);
+        List<Alarm> alarmList = List.of(alarm1, alarm2, alarm3);
+        Slice<Alarm> alarmSlice = new SliceImpl<>(alarmList, PageRequest.of(0, limit + 1), true);
 
         given(alarmRepository.getAllAlarmsDesc(any(), any())).willReturn(alarmSlice);
         given(alarmRepository.countAlarmsByUserId(userId)).willReturn(10L);
+
         given(alarmMapper.alarmToNotificationDto(any())).willReturn(mock(NotificationDto.class));
 
         // when
@@ -69,14 +73,12 @@ class AlarmServiceGetListTest {
 
         // then
         assertAll(
-                () -> assertEquals(2, result.contents().size(), "조회된 콘텐츠 개수가 일치해야 함"),
-                () -> assertEquals(10L, result.totalElements(), "전체 요소 개수가 일치해야 함"),
-                () -> assertTrue(result.hasNext(), "다음 페이지 여부가 true여야 함"),
-                () -> assertEquals(lastId.toString(), result.nextCursor(), "마지막 데이터의 ID가 nextCursor가 되어야 함"),
-                () -> assertEquals(lastCreatedAt, result.nextAfter(), "마지막 데이터의 createdAt이 nextAfter가 되어야 함")
+                () -> assertEquals(limit, result.contents().size(), "결과 리스트는 limit 크기만큼 잘려야 함"),
+                () -> assertTrue(result.hasNext(), "데이터가 limit보다 많으므로 hasNext는 true여야 함"),
+                () -> assertEquals(lastId.toString(), result.nextCursor(), "limit 번째 데이터의 ID가 커서가 되어야 함"),
+                () -> assertEquals(lastCreatedAt, result.nextAfter(), "limit 번째 데이터의 시간이 nextAfter가 되어야 함"),
+                () -> assertEquals(10L, result.totalElements())
         );
-
-        verify(alarmRepository).getAllAlarmsDesc(eq(request), any(Pageable.class));
     }
 
     @Test
