db15e58c0691061fee4b5bfc327aa5d2765910f8
yeeun000 <dldpdms0000@naver.com>
2026-03-09 11:35:44 +0900

    fix : 리뷰 매핑 오류 해결

--- src/main/java/com/redhorse/deokhugam/domain/review/mapper/ReviewMapper.java
+++ src/main/java/com/redhorse/deokhugam/domain/review/mapper/ReviewMapper.java
@@ -2,7 +2,6 @@ package com.redhorse.deokhugam.domain.review.mapper;
 
 import com.redhorse.deokhugam.domain.review.dto.ReviewDto;
 import com.redhorse.deokhugam.domain.review.entity.Review;
-import org.mapstruct.InheritConfiguration;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 
@@ -13,11 +12,15 @@ public interface ReviewMapper {
   @Mapping(source = "book.title", target = "bookTitle")
   @Mapping(source = "book.thumbnailUrl", target = "bookThumbnailUrl")
   @Mapping(source = "user.id", target = "userId")
-  @Mapping(source = "user.nickname", target="userNickname")
+  @Mapping(source = "user.nickname", target = "userNickname")
   @Mapping(target = "likedByMe", ignore = true)
   ReviewDto toDto(Review review);
 
-  @InheritConfiguration(name = "toDto")
+  @Mapping(source = "review.book.id", target = "bookId")
+  @Mapping(source = "review.book.title", target = "bookTitle")
+  @Mapping(source = "review.book.thumbnailUrl", target = "bookThumbnailUrl")
+  @Mapping(source = "review.user.id", target = "userId")
+  @Mapping(source = "review.user.nickname", target = "userNickname")
   @Mapping(source = "likedByMe", target = "likedByMe")
   ReviewDto toDto(Review review, boolean likedByMe);
 }
