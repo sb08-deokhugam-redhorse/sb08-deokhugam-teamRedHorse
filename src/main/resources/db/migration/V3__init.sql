aa806c89da8b76f04c99bffef6acc07537c1e321
dev-jin8612 <workspace8612@gmail.com>
2026-03-09 15:01:52 +0900

    refactor: DDL 오타 수정

--- src/main/resources/db/migration/V3__init.sql
+++ src/main/resources/db/migration/V3__init.sql
@@ -1,8 +1,2 @@
--- 컬럼명 변경 (thumbnail_url -> thumbnail_key)
-ALTER TABLE books RENAME COLUMN thumbnail_url TO thumbnail_key;
-
--- 컬럼 데이터 타입 및 사이즈 변경 (VARCHAR(255) -> VARCHAR(100))
-ALTER TABLE books ALTER COLUMN thumbnail_key TYPE VARCHAR(100);
-
 -- score 컬럼 타입을 BIGINT에서 DOUBLE PRECISION으로 변경
 ALTER TABLE popular_books ALTER COLUMN score TYPE DOUBLE PRECISION;
