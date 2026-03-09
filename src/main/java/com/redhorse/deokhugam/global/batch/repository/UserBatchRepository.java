66448fdefa39b5c430587a0d19acc41eb020d6ee
castle-bird <seongjo0501@gmail.com>
2026-03-09 15:19:21 +0900

    refactor: 삭제 시간 기준 변경

--- src/main/java/com/redhorse/deokhugam/global/batch/repository/UserBatchRepository.java
+++ src/main/java/com/redhorse/deokhugam/global/batch/repository/UserBatchRepository.java
@@ -13,7 +13,7 @@ public interface UserBatchRepository extends JpaRepository<User, UUID> {
   @Query(value = """
       DELETE FROM users 
       WHERE is_deleted = TRUE 
-      AND deleted_at < CURRENT_TIMESTAMP - INTERVAL '1 minute'
+      AND deleted_at < CURRENT_TIMESTAMP - INTERVAL '1 hour'
       """, nativeQuery = true)
   int deleteSoftDeletedUsersInBulk();
 }
