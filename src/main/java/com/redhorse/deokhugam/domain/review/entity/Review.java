e5466c7dfa8b94eae97b4f5a3eec37e78f59ce3e
yeeun000 <dldpdms0000@naver.com>
2026-03-10 13:28:55 +0900

    fix : 댓글 추가/삭제 시 리뷰 DB의 댓글 수 반영

--- src/main/java/com/redhorse/deokhugam/domain/review/entity/Review.java
+++ src/main/java/com/redhorse/deokhugam/domain/review/entity/Review.java
@@ -99,4 +99,14 @@ public class Review extends BaseUpdatableEntity {
       this.likeCount--;
     }
   }
+
+  public void incrementCommentCount() {
+    this.commentCount++;
+  }
+
+  public void decrementCommentCount() {
+    if (this.commentCount > 0) {
+      this.commentCount--;
+    }
+  }
 }
