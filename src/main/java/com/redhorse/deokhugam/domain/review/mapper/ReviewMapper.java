ed41967b5c19b53320dae959e3d3d9c371eedb19
yeeun000 <dldpdms0000@naver.com>
2026-03-11 09:30:48 +0900

    fix : 리뷰 조회 시 책 이미지 표시

--- src/main/java/com/redhorse/deokhugam/domain/review/mapper/ReviewMapper.java
+++ src/main/java/com/redhorse/deokhugam/domain/review/mapper/ReviewMapper.java
@@ -2,15 +2,16 @@ package com.redhorse.deokhugam.domain.review.mapper;
 
 import com.redhorse.deokhugam.domain.review.dto.ReviewDto;
 import com.redhorse.deokhugam.domain.review.entity.Review;
+import com.redhorse.deokhugam.infra.s3.S3ImageStorage;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 
-@Mapper(componentModel = "spring")
+@Mapper(componentModel = "spring", uses = {S3ImageStorage.class})
 public interface ReviewMapper {
 
   @Mapping(source = "book.id", target = "bookId")
   @Mapping(source = "book.title", target = "bookTitle")
-  @Mapping(source = "book.thumbnailUrl", target = "bookThumbnailUrl")
+  @Mapping(source = "review.book.thumbnailUrl", target = "bookThumbnailUrl", qualifiedByName = "toPresignedUrl")
   @Mapping(source = "user.id", target = "userId")
   @Mapping(source = "user.nickname", target = "userNickname")
   @Mapping(target = "likedByMe", ignore = true)
@@ -18,7 +19,7 @@ public interface ReviewMapper {
 
   @Mapping(source = "review.book.id", target = "bookId")
   @Mapping(source = "review.book.title", target = "bookTitle")
-  @Mapping(source = "review.book.thumbnailUrl", target = "bookThumbnailUrl")
+  @Mapping(source = "review.book.thumbnailUrl", target = "bookThumbnailUrl", qualifiedByName = "toPresignedUrl")
   @Mapping(source = "review.user.id", target = "userId")
   @Mapping(source = "review.user.nickname", target = "userNickname")
   @Mapping(source = "likedByMe", target = "likedByMe")
