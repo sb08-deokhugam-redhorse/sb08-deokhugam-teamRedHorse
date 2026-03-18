0bd602c5919d55069da177366b0b7917c88e5664
parkhamin <parkhamint@naver.com>
2026-03-18 16:58:57 +0900

    refactor: 댓믈 목록 조회 최적화

--- src/main/java/com/redhorse/deokhugam/domain/comment/repository/CommentRepository.java
+++ src/main/java/com/redhorse/deokhugam/domain/comment/repository/CommentRepository.java
@@ -1,15 +1,12 @@
 package com.redhorse.deokhugam.domain.comment.repository;
 
 import com.redhorse.deokhugam.domain.comment.entity.Comment;
-import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
 public interface CommentRepository extends JpaRepository<Comment, UUID>, CommentRepositoryCustom {
 
   Optional<Comment> findByIdAndDeletedAtIsNull(UUID commentId);
 
-  long countByReviewIdAndDeletedAtIsNull(UUID reviewId);
 }
